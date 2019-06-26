1bef58dc90f38af7ef8dca37492c05b2d51053d5
Richard Gomes <rgomes.info@gmail.com>
2019-06-26 22:39:31 +0200

    Support for SBT 1.x; Scala 2.12; ScalaJS 1.0.0-M8

--- src/main/scala/sri/sbt/platform/SriPlatformPlugin.scala
+++ src/main/scala/sri/sbt/platform/SriPlatformPlugin.scala
@@ -11,25 +11,25 @@ import sbt.Keys._
 object SriPlatformPlugin extends AutoPlugin {
   override lazy val requires = ScalaJSPlugin
   object autoImport {
-    lazy val common = config("common")
-    lazy val ios = config(IOS) extend (common)
-    lazy val android = config(ANDROID) extend (common)
-    lazy val web = config(WEB) extend (common)
-    lazy val CustomCompile = config("compile") extend (ios, android, common)
+    lazy val Platform = config("platform")
+    lazy val PlatformIOS = config(IOS) extend (Platform)
+    lazy val PlatformAndroid = config(ANDROID) extend (Platform)
+    lazy val PlatformWeb = config(WEB) extend (Platform)
+    lazy val CustomCompile = config("compile") extend (PlatformIOS, PlatformAndroid, Platform)
   }
 
   import autoImport._
 
   override def projectSettings: Seq[_root_.sbt.Def.Setting[_]] =
     Seq(
-      defaultConfiguration := Some(common),
+      defaultConfiguration := Some(Platform),
       scalaJSLinkerConfig ~= (_.withModuleKind(ModuleKind.CommonJSModule)),
-      ivyConfigurations := overrideConfigs(ios,
-                                           android,
-                                           web,
-                                           common,
+      ivyConfigurations := overrideConfigs(PlatformIOS,
+                                           PlatformAndroid,
+                                           PlatformWeb,
+                                           Platform,
                                            CustomCompile)(
         ivyConfigurations.value),
       scalaJSUseMainModuleInitializer := true
-    ) ++ buildConfig(android) ++ buildConfig(ios) ++ buildConfig(web)
+    ) ++ buildConfig(PlatformAndroid) ++ buildConfig(PlatformIOS) ++ buildConfig(PlatformWeb)
 }
